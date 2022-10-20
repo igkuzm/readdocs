6317b01838f91d377a3ff7041600e036d4999527
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-10-20 23:51:27 +1000

    docx

--- docx.c
+++ docx.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include "zip.h"
 #include "readdocs.h"
-
+#include "ezxml.h"
 
 /*
  * open docx and return it's text with NULL-terminated char array
@@ -20,7 +20,7 @@ readdocs_docx(const char * filename)
 {
 	//open file with zip
 	struct zip_t * zip =
-		zip_open(filename,  ZIP_DEFAULT_COMPRESSION_LEVEL, 'r');
+			zip_open(filename,  ZIP_DEFAULT_COMPRESSION_LEVEL, 'r');
 	if (!zip)
 		return NULL;
 
@@ -37,12 +37,31 @@ readdocs_docx(const char * filename)
 		return NULL;
 	}
 
-	//add NULL-termination to buffer
-	buf = realloc(buf, bufsize + 1);
-	if(!buf)
+	//allocate text
+	char * text = (char *)malloc(bufsize * sizeof(char));
+	if (!text)
 		return NULL;
-	((char *)buf)[bufsize] = 0;
 
-	//return buf
-	return buf;
+	//parse XML
+	ezxml_t xml = ezxml_parse_str((char*)buf, bufsize);
+	if (!xml)
+		return NULL;
+
+	//fill text with data
+	size_t i = 0;
+	ezxml_t body, p, r, t;
+	for (body = ezxml_child(xml, "w:body"); body; body = body->next)
+		for (p = ezxml_child(body, "w:p"); p; p = p->next){
+			text[i++] =	'\n';
+			for (r = ezxml_child(p, "w:r"); r; r = r->next)
+				for (t = ezxml_child(r, "w:t"); t; t = t->next){
+					char * ptr = t->txt;
+					while(*ptr)
+						text[i++] =	*ptr++;
+				}
+	}
+	text[i] = 0;
+
+	//return text
+	return text;
 }
